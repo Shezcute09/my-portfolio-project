9c6d6a10190e6ecc37da576a95d4ffc684a70eb9
Shezcute09 <chiemezieok3@gmail.com>
2024-06-09 22:33:59 +0100

    my first commit

---
+++ src/components/RelevantSkills.jsx
@@ -0,0 +1,5 @@
+const RelevantSkills = () => {
+  return <div></div>;
+};
+
+export default RelevantSkills;
---
+++ src/main.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Home from "./components/Home";
+import Article from "./components/Article";
+import ContactMe from "./components/ContactMe";
+import Experience from "./components/Experience";
+import Project from "./components/Project";
+import RelevantSkills from "./components/RelevantSkills";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/article",
+    element: <Article />,
+  },
+  {
+    path: "/contact-me",
+    element: <ContactMe />,
+  },
+  {
+    path: "/experience",
+    element: <Experience />,
+  },
+  {
+    path: "/project",
+    element: <Project />,
+  },
+  {
+    path: "/Relevant-Skills",
+    element: <RelevantSkills />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
